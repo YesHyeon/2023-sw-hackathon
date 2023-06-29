aea18627a74f3b7b46f173c59e56deda99820bdd
phjjj <aaagowns@gmail.com>
2023-06-29 17:56:10 +0900

    검색스크린
    검색스크린

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './pages/Login/Login';
-import KakaoRedirectHandler from './components/KakaoRedirectHandler';
-import Community from './pages/Community/Community';
-import Home from './pages/Home/Home';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login/Login";
+import KakaoRedirectHandler from "./components/KakaoRedirectHandler";
+import Community from "./pages/Community/Community";
+import Home from "./pages/Home/Home";
+import Search from "./pages/Search/Search";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/login" element={<KakaoRedirectHandler />} />
-        <Route path="/community" element={<Community />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/login" element={<KakaoRedirectHandler />} />
+                <Route path="/community" element={<Community />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/search" element={<Search />} />
+            </Routes>
+        </Router>
+    );
 }
 export default App;
---
+++ client/src/pages/Search/Search.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import {
+    Btn,
+    Box,
+    Container,
+    Input,
+    BigTitle,
+    Wraaper,
+    Title,
+    Contents,
+} from "./Search.style";
+
+function Search() {
+    const [value, setValue] = useState("");
+    const onChange = (event: any) => setValue(event.target.value);
+    return (
+        <Wraaper>
+            <Container>
+                <Box>
+                    <BigTitle>
+                        <Title>가치가요</Title>
+                        <Contents>
+                            병 이름을 입력하시면 우리 집 근처, 가장 알맞은
+                            병원을 추천해드려요!
+                        </Contents>
+                    </BigTitle>
+                    <Input
+                        onChange={onChange}
+                        type="text"
+                        value={value}
+                        placeholder="입력"
+                    />
+                    <Btn type="submit" disabled={!value}>
+                        병원찾기
+                    </Btn>
+                </Box>
+            </Container>
+        </Wraaper>
+    );
+}
+export default Search;
