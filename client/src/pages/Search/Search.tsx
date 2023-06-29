ac4d1fe8c47c55a308b44d720b8ca47d1e150172
shgus1224 <shgus1224@naver.com>
2023-06-29 21:08:47 +0900

    feat: 현재위치 불러오기 기능 추가

--- client/src/pages/Search/Search.tsx
+++ client/src/pages/Search/Search.tsx
@@ -1,41 +1,73 @@
-import { useState } from "react";
+import { useState, useCallback, useEffect } from 'react';
 import {
-    Btn,
-    Box,
-    Container,
-    Input,
-    TitleBox,
-    Wraaper,
-    Title,
-    Contents,
-} from "./Search.style";
+  Btn,
+  Box,
+  Container,
+  Input,
+  TitleBox,
+  Wraaper,
+  Title,
+  Contents,
+} from './Search.style';
+import useGeolocation from 'react-hook-geolocation';
 
 function Search() {
-    const [value, setValue] = useState("");
-    const onChange = (event: any) => setValue(event.target.value);
-    return (
-        <Wraaper>
-            <Container>
-                <Box>
-                    <TitleBox>
-                        <Title>가치가요</Title>
-                        <Contents>
-                            병 이름을 입력하시면 우리 집 근처, 가장 알맞은
-                            병원을 추천해드려요!
-                        </Contents>
-                    </TitleBox>
-                    <Input
-                        onChange={onChange}
-                        type="text"
-                        value={value}
-                        placeholder="입력"
-                    />
-                    <Btn type="submit" disabled={!value}>
-                        병원찾기
-                    </Btn>
-                </Box>
-            </Container>
-        </Wraaper>
-    );
+  const geolocation = useGeolocation();
+  const [value, setValue] = useState('');
+  const onChange = (event: any) => setValue(event.target.value);
+
+  const getLocation = useCallback(() => {
+    const getCurrentPosBtn = () => {
+      navigator.geolocation.getCurrentPosition(
+        getPosSuccess,
+        () => alert('위치 정보를 가져오는데 실패했습니다.'),
+        {
+          enableHighAccuracy: true,
+          maximumAge: 30000,
+          timeout: 27000,
+        }
+      );
+    };
+
+    const getPosSuccess = (pos: GeolocationPosition) => {
+      console.log(pos.coords.longitude);
+      console.log(pos.coords.latitude);
+      // 현재 위치(위도, 경도) 가져온다.
+
+      //   setA({ lat: pos.coords.latitude, lng: pos.coords.longitude });
+      //   setMapLocation({ lat: pos.coords.latitude, lng: pos.coords.longitude });
+    };
+
+    getCurrentPosBtn();
+  }, []);
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
+  return (
+    <Wraaper>
+      <Container>
+        <Box>
+          <TitleBox>
+            <Title>가치가요</Title>
+            <Contents>
+              병 이름을 입력하시면 우리 집 근처, 가장 알맞은 병원을
+              추천해드려요!
+            </Contents>
+          </TitleBox>
+          <Input
+            onChange={onChange}
+            type="text"
+            value={value}
+            placeholder="입력"
+          />
+          <Btn type="submit" disabled={!value}>
+            병원찾기
+          </Btn>
+        </Box>
+      </Container>
+    </Wraaper>
+  );
 }
 export default Search;
